0f1e29089ee6806354e8095bc422f090c7edd11b
saïna <saina.lalaniaina@gmail.com>
2026-06-04 21:24:14 +0300

    js à propos

---
+++ about.js
@@ -0,0 +1,15 @@
+const currentUser = localStorage.getItem("currentUser");
+const userDisplayAbout = document.getElementById("user-display-about");
+
+if (currentUser) {
+    if (userDisplayAbout) {
+        userDisplayAbout.innerHTML =
+            '<span id="user-name">👤 ' + currentUser + '</span>' +
+            '<button id="logout-btn" onclick="logout()">Déconnexion</button>';
+    }
+    
+} else {
+    if (userDisplayAbout) {
+        userDisplayAbout.innerHTML = '<a href="login.html" class="btn-connexion">Connexion</a>';
+    }
+}
